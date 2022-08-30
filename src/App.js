70ab565513e92b21a75beaf5f59e3c0b1d0f3a98
techie <codeer0@gmail.com>
2022-08-30 13:21:32 +0530

    compleated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from "react"
+import logo from "./logo.png"
 function App() {
+
+  const [password,setPassword] = useState(0)
+
+  function getPassword(length){
+  var result = ""; 
+  var keys = "ABABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz12ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+{}|:?<>?34567890~!@#$%^&*()_+{}|:?<>?CDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+{}|:?<>?";
+  var charLength = keys.length; 
+
+  for(var i = 0; i<length;i++){
+      result+=keys.charAt(Math.floor(Math.random()*charLength));
+  
+  }
+  return result;
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo}/>
+      <h2>Password Generater</h2>
+        <input placeholder="Enter length of password" onChange={event=>setPassword(event.target.value)} name="length" min="8" max="99"/>
+        
+      <p>{getPassword(password)}</p>
+
+    <b>Password contain</b>
+      <i><ul>
+        <li>Upper case</li>
+        <li>Lower case</li>
+        <li>Numbers </li>
+        <li>Special Characters</li>
+      </ul></i>
+      
       </header>
     </div>
   );
